e7fccd99c45c632a3bd9db55c30fd5e21aa74a05
ryanabest <ryanabest@gmail.com>
2023-07-16 22:06:55 -0400

    update webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
@@ -15,7 +15,7 @@ module.exports = merge(common, {
   },
   optimization: {
     minimizer: [
-      new OptimizeCssAssetsPlugin(),
+      new CssMinimizerPlugin(),
       new TerserPlugin()
     ]
   },
