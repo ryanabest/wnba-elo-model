3e8106655eac1d351e7eca4f34433f546ea24f6d
rbest <rbest@ap.org>
2023-07-24 16:47:51 -0400

    render games on front end

--- src/js/games.js
+++ src/js/games.js
@@ -5,9 +5,44 @@ class Games {
   constructor () {
     this.games = games;
     this.processed_games = utils.games.processGames(games);
+    this.cutoff = 7;
 
+    this.setTemplates();
+    this.render();
     this.addUpcomingEventListener();
     this.addCompletedEventListener();
+  }
+
+  setTemplates() {
+    this.games_recent = require('../templates/games/games-recent.pug');
+    this.games_upcoming = require('../templates/games/games-upcoming.pug');
+    this.games_completed = require('../templates/games/games-completed.pug');
+  }
+
+  render () {
+    const opts = {
+      processedGames: this.processed_games,
+      cutoff: this.cutoff,
+      utils
+    };
+
+    const numRecent = Object.keys(this.processed_games.recent).length;
+    if (numRecent > 0) {
+      const html = this.games_recent(opts);
+      document.querySelector('#recent-games').innerHTML = html;
+    }
+
+    const numPre = Object.keys(this.processed_games.pre).length;
+    if (numPre > 0) {
+      const html = this.games_upcoming(opts);
+      document.querySelector('#upcoming-games').innerHTML = html;
+    }
+
+    const numPost = Object.keys(this.processed_games.post).length;
+    if (numPost > 0) {
+      const html = this.games_completed(opts);
+      document.querySelector('#completed-games').innerHTML = html;
+    }
   }
 
   showMoreGames (type) {
