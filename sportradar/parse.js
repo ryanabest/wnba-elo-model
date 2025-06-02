a131308c11f6f4825b768374f8cd9dbd82cac54a
ryanabest <ryanabest@gmail.com>
2025-06-02 09:33:44 -0400

    updates

--- sportradar/parse.js
+++ sportradar/parse.js
@@ -11,21 +11,21 @@ runner.run();
 // ~~ TODO: CLINCH SCRAPER RUNS ASYNC, SO BY THE TIME THIS SCRIPT GETS HERE IT'S NOT FINISHED
 // ~~ ACTUALLY SCRAPE CLINCHES FIRST, THEN PARSE THEM
 // if (runner.should_deploy || clinchScraper.should_deploy) {
-if (runner.should_deploy) {
-  const webpack = require('webpack');
-  const config = require('../webpack.prod');
-  const compiler = webpack(config);
-  const ghpages = require('gh-pages');
+// if (runner.should_deploy) {
+//   const webpack = require('webpack');
+//   const config = require('../webpack.prod');
+//   const compiler = webpack(config);
+//   const ghpages = require('gh-pages');
   
-  // ~~ first compile webpack ~~ //
-  compiler.run((err, res) => {
-    if (err) console.log(err);
+//   // ~~ first compile webpack ~~ //
+//   compiler.run((err, res) => {
+//     if (err) console.log(err);
 
-    // // ~~ then publish on github pages ~~ //
-    ghpages.publish('dist', {
-      repo: `git@github.com:ryanabest/${config.season}-wnba-predictions.git`
-    }, (err) => {
-      console.log(err);
-    });
-  });
-}
+//     // // ~~ then publish on github pages ~~ //
+//     ghpages.publish('dist', {
+//       repo: `git@github.com:ryanabest/${config.season}-wnba-predictions.git`
+//     }, (err) => {
+//       console.log(err);
+//     });
+//   });
+// }
